f75cdd53c808682b27320e10b267d5ed9d09aba6
jmacey <jmacey@bournemouth.ac.uk>
2015-10-09 14:38:23 +0100

    Removed NGLQuit as deprecated

--- SpherePlane/src/NGLScene.cpp
+++ SpherePlane/src/NGLScene.cpp
@@ -54,9 +54,7 @@ NGLScene::NGLScene(int _numSpheres)
 
 NGLScene::~NGLScene()
 {
-  ngl::NGLInit *Init = ngl::NGLInit::instance();
   std::cout<<"Shutting down NGL, removing VAO's and Shaders\n";
-  Init->NGLQuit();
 }
 
 void NGLScene::resizeGL(int _w, int _h)
